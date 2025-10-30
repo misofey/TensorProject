3876ae652ba9bfccde791439438dd7ed95ee1424
Miso Fey <feymiso@gmail.com>
2025-10-30 20:44:20 +0100

    silrtc debugged

--- tensor_reconstruction.m
+++ tensor_reconstruction.m
@@ -2,60 +2,73 @@ function [] = tensor_reconstruction()
 %TENSOR_RECONSTRUCTION Summary of this function goes here
 %   Detailed explanation goes here
     [image, M, N] = load_image();
-    n = ndim(image);
+    n = ndims(image);
     addpath("utils/");
-    [omega, omega_overline, removed_idx, kept_idx] = create_masks(0.1)
-    x_0 = zeros(M, N, 3);
-    x_0(kept_idx(:, 1), kept_idx(:, 2), :) = image(kept_idx(:, 1), kept_idx(:, 2), :);
-    omega_1 = mode_n_matricization(omega, 1);
-    omega_2 = mode_n_matricization(omega, 2);
-    omega_3 = mode_n_matricization(omega, 3);
-    X_1 = mode_n_matricization(x_0, 1);
-    X_2 = mode_n_matricization(x_0, 2);
-    X_3 = mode_n_matricization(x_0, 3);
-    M = [X_1, X_2, X_3];
-    alpha = ones(1, n) / n;
-    beta = ones(1, n);
+    [omega, omega_overline, removed_idx, kept_idx, lin_rem_idx] = create_masks(0.1);
+    X_0 = double(image);
+    X_0(lin_rem_idx) = 0;
+    X_1 = mode_n_matricization(X_0, 1);
+    X_2 = mode_n_matricization(X_0, 2);
+    X_3 = mode_n_matricization(X_0, 3);
+    alphas = ones(1, n) / n;
+    betas = ones(1, n) / 100;
+    rho = 1;
+    T = {mode_n_matricization(image, 1), mode_n_matricization(image, 2), mode_n_matricization(image, 3)};
+    M = {X_1, X_2, X_3};
+    X = X_0;
+    Y = {zeros(1, like=X_1), zeros(1, like=X_2), zeros(1, like=X_3)};
+
+    [reconstructed, errors] = silrtc(M, X, alphas, betas, n, lin_rem_idx, 100, T, image);
+    plot(errors(:, 4))
 end
 
 function [image, M, N] = load_image()
-    tiffObj = Tiff("image1.tiff", 'r');
-    image = imread(tiffObj);
+    image = imread("image1.tiff", "tiff");
+    image = image(1:256, 1:256, :);
     sz = size(image);
-    [M, N] = sz(1:2);
+    M = sz(1);
+    N = sz(2);
 end
 
-function [omega, removed_idx, kept_idx] = create_masks(removed_percentage)
+function [omega, omega_overline, removed_idx, kept_idx, lin_rem_idx] = create_masks(removed_percentage)
     omega = [];
     omega_overline = [];
     removed_idx = [];
     kept_idx = [];
-    image_size = 500;
+    image_size = 256;
     pixel_count = image_size*image_size;
     no_removed = round(pixel_count*removed_percentage);
     removed_flattened = randperm(pixel_count, no_removed);  % idx to remove from the flattened array
-    removed_idx = zeros(no_removed, 2,  "int64");
-    removed_idx(:, 1) = idivide(int64(removed_flattened), int64(image_size))+1;  % width removed ones
-    removed_idx(:, 2) = (mod(removed_flattened, image_size))+1;  % height removed ones
-    omega = ones(500);
+    removed_idx = zeros(no_removed, 2,  "int32");
+    [removed_idx(:, 1), removed_idx(:, 2)] = ind2sub([image_size, image_size], removed_flattened);
+    omega = ones(image_size);
     omega(removed_idx(:, 1), removed_idx(:, 2)) = 0;
-    omega = cat(3, omega, omega, omega);
     omega_overline = 1-omega;
-    [rk, ck] = find(omega(:,:,1) == 1);
+    [rk, ck] = find(omega(:,:) == 1);
     kept_idx = int64([rk ck]);
+    lin_rem_idx = [removed_flattened, removed_flattened, removed_flattened];
 end
     
-function [X] = silrtc(M, X, alphas, betas, n, idx_removed, K)
+function [X, errors] = silrtc(M, X, alphas, betas, n, lin_rem_idx, K, T, image)
     % idx removed are the pixels removed from the image
+    errors = ones(K, 4);
     for j = 1:K
-        selection = @(m) m(idx_removed(:, 1), idx_removed(:, 2), :);
-        selected = zeros(length(idx_removed, 1));  % for collecting the average of all versions of the optimizaiton variables
+        disp("beginning iteration");
+        selected = zeros(length(lin_rem_idx),1)';  % for collecting the average of all versions of the optimizaiton variables
         for i = 1:n
-            M(i) = shrinkage(mode_n_matricization(X, i), alphas(i)/betas(i));
-            folded = mode_n_folding(M(i));
-            selected = selected + selection(folded) * betas(i);
+            disp(["beginning update of block matrix", num2str(n)]);
+            drawnow('update');
+            M{i} = shrinkage(mode_n_matricization(X, i), alphas(i)/betas(i));
+            folded = mode_n_folding(M{i}, i, size(X));
+            selected = selected + folded(lin_rem_idx) * betas(i);
+
         end
-        X(idx_removed(:, 1), idx_removed(:, 2), :) = selected / sum(betas);
+
+        X(lin_rem_idx) = selected / sum(betas);
+        for asdf = 1:3
+            errors(j, asdf) = frob_norm(double(T{asdf})-M{asdf});
+        end
+        errors(j, 4) = frob_norm(double(image) - X);
     end
 end
 
@@ -77,9 +90,9 @@ end
 
 function [D] = shrinkage(X, tau)
     %% rounds tau down to the lowest nu
-    [U, S, V] = svd(X);
-    S_tau = max(S - tau, 0);
-    D = U * S * V';
+    [U, S, V] = svd(X, "econ");
+    S_tau = diag(max(diag(S) - tau, 0));
+    D = U * S_tau * V';
 end
 
 function [T] = truncation(X, tau)
